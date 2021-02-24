6c1ebd99a6a6876b7cdebd23aa03cf0d1a7ea6ff
yaswanthuddagiri <79571999+yaswanthuddagiri@users.noreply.github.com>
2021-02-24 15:41:37 +0530

    adding the file

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c,sum;
+  printf("enter any three values");
+  scanf("%d%d%d",&a,&b,&c);
+  sum=a+b+c;
+  printf("the sum of the three number is",sum);
+}
+https://github.com/yaswanthuddagiri/T20MCA_GITHUB/blob/main/README.md
